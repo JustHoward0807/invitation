1cf64c9d7565b3e598e0cb47616e8ce18f782d4f
Howard Tung <u1422283@umail.utah.edu>
2024-11-05 20:21:29 -0700

    HUG Content

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import emailjs from '@emailjs/browser';
+import {motion, TapInfo} from "framer-motion"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    function sendEmail(event: MouseEvent | TouchEvent | PointerEvent, info: TapInfo) {
+        emailjs.init({
+            publicKey: "BVBZ8cjcS1Azpzgaq"
+        })
+
+        emailjs.send(
+            "service_p62y7os",
+            "template_zbq8wt5"
+        ).then(r => alert("收到！ 🥰🥰🥰"))
+    }
+
+    return (
+        <div className="App">
+
+            <img
+                src="https://media1.tenor.com/m/OHWzrmXXV5EAAAAd/needhug-migration.gif"
+                alt="HUG JC"/>
+
+            <div style={{marginTop: '20px'}}>
+                <motion.button
+                    onTap={sendEmail}
+                    className="box"
+                    style={{width: '100px', height: '40px'}}  // Increased height for a more balanced look
+                    whileHover={{scale: 1.1}}
+                    whileTap={{scale: 0.9}}
+                    transition={{type: "spring", stiffness: 400, damping: 10}}
+                >
+                    HUG 🥰
+                </motion.button>
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
