45a2fff5c54ae0f115fca9d3ffc2a908fa364fb1
Howard Tung <u1422283@umail.utah.edu>
2024-11-06 20:19:26 -0700

    Dinner invitation

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,88 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import emailjs from '@emailjs/browser';
 import {motion, TapInfo} from "framer-motion"
 
 function App() {
-    function sendEmail(event: MouseEvent | TouchEvent | PointerEvent, info: TapInfo) {
+    const [count, setCount] = React.useState(1);
+    let limit: number = 3;
+    const [showYesMessage, setShowYesMessage] = React.useState<boolean>(false);
+    const [showNoMessage, setShowNoMessage] = React.useState<boolean>(false);
+
+    function sendEmail(event: MouseEvent | TouchEvent | PointerEvent, info: TapInfo, isMaybe: boolean) {
         emailjs.init({
             publicKey: "BVBZ8cjcS1Azpzgaq"
         })
 
-        emailjs.send(
-            "service_p62y7os",
-            "template_zbq8wt5"
-        ).then(r => alert("收到！ 🥰🥰🥰"))
+        if (isMaybe && count < 4) {
+            emailjs.send(
+                "service_p62y7os",
+                "template_qiwqlv7",
+                {message: count}
+            ).then(r => {
+                setCount(count + 1)
+                if (count === limit) {
+                    setShowNoMessage(true);
+                    setShowYesMessage(false);
+                }
+            });
+        } else {
+            emailjs.send(
+                "service_p62y7os",
+                "template_zbq8wt5"
+            ).then(r => {
+                setShowYesMessage(true);
+                setShowNoMessage(false)
+            })
+        }
+
     }
 
     return (
-        <div className="App">
-
-            <img
-                src="https://media1.tenor.com/m/OHWzrmXXV5EAAAAd/needhug-migration.gif"
-                alt="HUG JC"/>
-
-            <div style={{marginTop: '20px'}}>
-                <motion.button
-                    onTap={sendEmail}
-                    className="box"
-                    style={{width: '100px', height: '40px'}}  // Increased height for a more balanced look
-                    whileHover={{scale: 1.1}}
-                    whileTap={{scale: 0.9}}
-                    transition={{type: "spring", stiffness: 400, damping: 10}}
-                >
-                    HUG 🥰
-                </motion.button>
+        <div className="blurred-background">
+            <div className="centered-content">
+                <img src="https://media1.tenor.com/m/whAhQj8irG8AAAAd/lady-and-the-tramp-dogs.gif"/>
             </div>
+
+            <h2 style={{color: "white", fontFamily: "Chalkboard"}}>Dinner with 哈沃學長</h2>
+            <div className="btn-box">
+                <div style={{marginTop: '20px'}}>
+                    <motion.button
+                        onTap={(event, info) => sendEmail(event, info, false)}
+                        className="box"
+                        style={{width: '100px', height: '40px'}}
+                        whileHover={{scale: 1.5, backgroundColor: '#52c41a'}}
+                        whileTap={{scale: 0.9}}
+                        transition={{type: "spring", stiffness: 400, damping: 10}}
+                    >
+                        YES 🥰
+                    </motion.button>
+                </div>
+
+                <div style={{marginTop: '20px', marginLeft: '20px'}}>
+                    <motion.button
+                        onTap={(event, info) => sendEmail(event, info, true)}
+                        className="box-maybe"
+                        style={{width: '100px', height: '40px'}}
+                        whileHover={{scale: 1.15, backgroundColor: '#d32f2f'}}
+                        whileTap={{scale: 0.9}}
+                        animate={{x: [-5, 5, -5]}}
+                        transition={{
+                            type: "spring",
+                            stiffness: 50,
+                            damping: 10,
+                            duration: 1.5
+                        }}
+                    >
+
+                        {count === 2 ? 'Are you sure?' : count === 3 ? '🥲 (One more time)' : 'MAYBE ❌'}
+                    </motion.button>
+                </div>
+            </div>
+
+
+            {showYesMessage ? <h1 style={{color: "white"}}>明智的選擇!</h1> : null}
+            {showNoMessage ? <h1 style={{color: "white"}}>好吧...</h1> : null}
 
         </div>
     );
